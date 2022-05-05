9952e5aaca8a6deed9ec9fdf52671a74ca4df6d5
Jatin2409 <jatinghind007@gmail.com>
2022-05-05 15:43:41 +0530

    Updated Source File

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import CasinoGame.*;
 public class Main{
 	public static void main(String[] args) {
-		MainGame obj = new MainGame();
-		obj.NumberGame();
+		MainGame myGame = new MainGame();
+		myGame.NumberGame();
 	}
 }
